bfc907af835c4d033d1af8bedc6ac55f56e3ac46
charline <charline.heuguet@gmail.com>
2023-10-06 16:05:28 +0200

    Màj html css et js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// cibler l'element btn qui va ouvrir la modal
+const btnOpen = document.querySelector('#btnOpen');
+
+const modal = document.querySelector('.modalContainer');
+
+function openModal() {
+    "Hello World"
+}
+// Au click sur btnOpen, déclenche la function openModal
+btnOpen.addEventListener("click", openModal());
